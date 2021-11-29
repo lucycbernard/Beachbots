79ed8c540f8633195b4e67a548ad4ddcd4bf379f
jacasella <casellaja@gmail.com>
2021-11-29 14:45:12 -0500

    Implemented first iteration of bounds detection to detect if any tag is out of a set boundary. TODO add support for multiple tags and boundaries

--- Code/catkin_ws/src/base_bot/src/BoundsDetection.py
+++ Code/catkin_ws/src/base_bot/src/BoundsDetection.py
@@ -10,7 +10,9 @@
 # ros parameters  : 
 # ==============================================================================
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import Bool
+import tf
+from apriltag_ros.msg import AprilTagDetectionArray, AprilTagDetection
 import math
 
 
@@ -18,10 +20,46 @@ import math
 class BoundsDetection:
 
     def __init__(self):
+        """"
+        Class constructor
+        """
 
+        # initialize ros node
+        rospy.init_node("BoundsDetection", anonymous=True)
 
-    
+        # get bounds from parameter service (for now hard code for a single tag)
+        #  lower and upper are the bounds in meters of the tag
+        self.lower = 0.4
+        self.upper = 0.6
+
+        # Create publisher for bounds data
+        self.pub = rospy.Publisher("/tag_bounds", Bool, queue_size=10)
+
+        # Create subscriber to listen to AprilTag data
+        rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.handleAprilTag)
+
+        # rate to process tags
+        Rate = 0.15
+
+        # seep to let ros initialize
+        rospy.sleep(2)
+
+        # loop until ros is shutdown
+        while not rospy.is_shutdown():
+            rospy.sleep(Rate)
+
+    def handleAprilTag(self, ATArray):
+        """
+        Reads the detected AprilTags and handles any that are out of bounds
+        :param ATArray         [apriltag_ros.msg.AprilTagDetectionArray] Array of detected AprilTags
+        """
+
+        for tag in ATArray.detections:
+            zDist = tag.pose.pose.pose.position.z
+            print(zDist)
+            if zDist < self.lower or zDist > self.upper:
+                self.pub.publish(True)
 
 
 if __name__ == "__main__":
-    
+    BD = BoundsDetection()
